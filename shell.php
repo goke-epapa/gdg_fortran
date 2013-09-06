c8b6230f3d573a47bbc1e46a5579880f67e6faf0
goke-epapa <adegokeobasa@gmail.com>
2013-09-06 04:58:49 +0100

    Implemented error catching from gfortran compiler

--- shell.php
+++ shell.php
@@ -8,11 +8,13 @@ $temp = "PROGRAM GOKE". "\n".
 "END PROGRAM GOKE";
 $string = isset($_POST["code"]) ? $_POST["code"] : $temp; 
 file_put_contents("test2.f90",$string);
-$error  = shell_exec("gfortran -o test2 test2.f90");
-echo $error;
+file_put_contents("test2.err","");
+shell_exec("gfortran -o test2 test2.f90 2> test2.err");
+$error = file_get_contents("test2.err");
 if(strlen($error) > 0){
 	echo $error;
+    exit();
 }
 $output = shell_exec('./test2');
 echo "<pre>$output</pre>";
-?>
+
